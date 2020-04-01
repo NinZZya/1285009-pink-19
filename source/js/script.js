13e1f9d0d89d6a082a4c78ed31c6d636f329cf82
ninzzya <s.a.azarov@gmail.com>
2020-04-01 23:02:05 +0300

    Добавляет переключение класса main-nav

--- source/js/script.js
+++ source/js/script.js
@@ -1,18 +1,22 @@
 const menuConfig = {
   menuId: "#menu",
-  menuClassClose: "menu--close",
+  menuClassActive: "menu--active",
   btnClass: ".logo__btn",
-  btnClassClose: "logo__btn--close"
+  btnClassClose: "logo__btn--close",
+  mainNavClass: ".main-nav",
+  mainNavClassActive: "main-nav--active"
 };
 
-
 var counter = 0;
-const mainMenu = document.querySelector(menuConfig.menuId);
-const btnMenu = document.querySelector(menuConfig.btnClass);
+
+const mainNav = document.querySelector(menuConfig.mainNavClass);
+const mainMenu = mainNav.querySelector(menuConfig.menuId);
+const btnMenu = mainNav.querySelector(menuConfig.btnClass);
 
 if (mainMenu && btnMenu) {
   btnMenu.addEventListener("click", function(evt){
-    mainMenu.classList.toggle(menuConfig.menuClassClose);
+    mainMenu.classList.toggle(menuConfig.menuClassActive);
     btnMenu.classList.toggle(menuConfig.btnClassClose);
+    mainNav.classList.toggle(menuConfig.mainNavClassActive);
   });
 }
